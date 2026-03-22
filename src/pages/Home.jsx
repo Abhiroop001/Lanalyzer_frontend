e6ba6d5f874abf0236e9e52e1428439aa6e8ebca
Abhiroop001 <Chandanamukherjee9804@gmail.com>
2026-03-22 15:31:02 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -73,7 +73,7 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8000/api/documents/upload", {
+      const res = await fetch("https://lanalyzer-backend.onrender.com/api/documents/upload", {
         method: "POST",
         body: formData,
       });
